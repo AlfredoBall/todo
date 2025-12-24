dc110c05c1893b1fdd1b74ec41bedc50c8110898
Alfredo Ball <alfredo.ball@outlook.com>
2025-12-24 02:49:52 -0600

    scope attempt 2

--- DevOps/Infrastructure/Terraform/api/outputs.tf
+++ DevOps/Infrastructure/Terraform/api/outputs.tf
@@ -3,16 +3,17 @@ output "api_app_service_default_url" {
   value       = "https://${azurerm_linux_web_app.api.name}.azurewebsites.net"
 }
 
-output "api_scope" {
-  description = "API scope for authentication (format: api://client-id/scope)"
-  value       = "api://${azuread_application.api_app_registration.client_id}/access_as_user"
-}
 output "api_app_registration_client_id" {
   value = azuread_application.api_app_registration.client_id
 }
 
+output "api_scope" {
+  description = "API scope for authentication (format: api://client-id/scope)"
+  value       = "api://${azuread_application.api_app_registration.client_id}/access_as_user"
+}
+
 output "api_scope_id" {
-  value = tolist(azuread_application.api_app_registration.oauth2_permission_scope)[0].id
+  value = tolist(azuread_application.api_app_registration.api.oauth2_permission_scope)[0].id
   description = "The UUID of the API scope for delegated permission. Use this for advanced authentication scenarios."
 }
 
