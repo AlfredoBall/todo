4737de20fdd489d8c5489b5c2560dc04da425df5
Alfredo Ball <alfredo.ball@outlook.com>
2025-12-24 12:38:51 -0600

    Create app insights workspace

--- DevOps/Infrastructure/Terraform/api/api_app_insights.tf
+++ DevOps/Infrastructure/Terraform/api/api_app_insights.tf
@@ -3,4 +3,13 @@ resource "azurerm_application_insights" "api" {
   location            = var.location
   resource_group_name = var.resource_group_name
   application_type    = "web"
+  workspace_id  = azurerm_log_analytics_workspace.api.id
+}
+
+resource "azurerm_log_analytics_workspace" "api" {
+  name                = "${var.api_app_service_name}-logs"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
 }
