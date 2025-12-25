02ddad9cd4b2cd6908de038d2ab655edf405a6c0
Alfredo Ball <alfredo.ball@outlook.com>
2025-12-24 22:00:24 -0600

    Remove Azure CORS

--- DevOps/Infrastructure/Terraform/api/api_web_app_service.tf
+++ DevOps/Infrastructure/Terraform/api/api_web_app_service.tf
@@ -13,14 +13,6 @@ resource "azurerm_linux_web_app" "api" {
       dotnet_version = "10.0"
     }
     always_on = false
-
-    # Configure App Service CORS origins to allow requests from the frontend web app
-    # cors {
-    #   allowed_origins = [
-    #     "https://${var.frontend_default_hostname}",
-    #   ]
-    #   support_credentials = true
-    # }
   }
 
   app_settings = {
