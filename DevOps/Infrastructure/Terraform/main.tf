472f567cbfc1b9454d72fc6786b095d95e40eee7
Alfredo Ball <alfredo.ball@outlook.com>
2025-12-27 06:17:16 -0600

    Pass variables to Terraform Apply - 14

--- DevOps/Infrastructure/Terraform/main.tf
+++ DevOps/Infrastructure/Terraform/main.tf
@@ -1,9 +1,17 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.resource_group_name}-${var.target_env}"
+  location = var.location
+  tags     = var.resource_tags
+}
+
 resource "azurerm_service_plan" "service_plan_linux" {
   name                = "${var.service_plan_linux_name}-${var.target_env}"
   location            = var.location
   resource_group_name = "${var.resource_group_name}-${var.target_env}"
   os_type             = "Linux"
   sku_name            = "F1"
+
+  depends_on = [ azurerm_resource_group.rg ]
 }
 
 resource "azurerm_service_plan" "service_plan_windows" {
@@ -12,12 +20,8 @@ resource "azurerm_service_plan" "service_plan_windows" {
   resource_group_name = "${var.resource_group_name}-${var.target_env}"
   os_type             = "Windows"
   sku_name            = "B1"
-}
 
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.resource_group_name}-${var.target_env}"
-  location = var.location
-  tags     = var.resource_tags
+  depends_on = [ azurerm_resource_group.rg ]
 }
 
 module "api" {
