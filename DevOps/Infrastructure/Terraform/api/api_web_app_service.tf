328c4dcfd987d3376aafe3d438879218fbca16cc
Alfredo Ball <alfredo.ball@outlook.com>
2025-12-26 21:42:49 -0600

    Pass variables to Terraform Apply - 10

--- DevOps/Infrastructure/Terraform/api/api_web_app_service.tf
+++ DevOps/Infrastructure/Terraform/api/api_web_app_service.tf
@@ -19,20 +19,23 @@ resource "azurerm_windows_web_app" "api" {
     always_on = true
   }
 
-  app_settings = {
-    "ASPNETCORE_ENVIRONMENT" = "${ title(var.target_env) }"
-    "FRONTEND_URL"             = "https://${var.frontend_default_hostname}"
+  app_settings = merge(
+  {
+    "ASPNETCORE_ENVIRONMENT"                      = title(var.target_env)
+    "FRONTEND_URL"                                = "https://${var.frontend_default_hostname}"
     "WEBSITES_DISABLE_APP_SERVICE_AUTHENTICATION" = "true"
-    # Azure AD configuration for token validation
-    "AzureAd__TenantId" = data.azuread_client_config.current.tenant_id
-    "AzureAd__ClientId" = azuread_application.api_app_registration.client_id
-    "AzureAd__Audience" = "api://${azuread_application.api_app_registration.client_id}"
-    "AzureAd__Instance" = "https://login.microsoftonline.com/"
-    "AzureAd__Scopes"   = "access_as_user"
-    # Application Insights connection string now set in app_settings
-    "ApplicationInsightsAgent_EXTENSION_VERSION" = "~3"
-    "APPLICATIONINSIGHTS_CONNECTION_STRING" = azurerm_application_insights.api.connection_string
-  }
+    "AzureAd__TenantId"                           = data.azuread_client_config.current.tenant_id
+    "AzureAd__ClientId"                           = azuread_application.api_app_registration.client_id
+    "AzureAd__Audience"                           = "api://${azuread_application.api_app_registration.client_id}"
+    "AzureAd__Instance"                           = "https://login.microsoftonline.com/"
+    "AzureAd__Scopes"                             = "access_as_user"
+    "ApplicationInsightsAgent_EXTENSION_VERSION"  = "~3"
+  },
+  # Only add this map if target_env is production
+  var.target_env == "production" ? {
+    "APPLICATIONINSIGHTS_CONNECTION_STRING" = azurerm_application_insights.api[0].connection_string
+  } : {}
+)
 }
 
 data "azuread_client_config" "current" {}
